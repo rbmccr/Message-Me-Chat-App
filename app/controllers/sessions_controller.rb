434f89fc60096b034e00f5451f0cc4118fab834d
Brendan McCray <rbmccr@gmail.com>
2019-11-13 11:42:27 -0600

    Added authentication. Log in and log out are functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,22 @@ class SessionsController < ApplicationController
 
     end
 
+    def create
+				user = User.find_by(username: params[:session][:username])
+				if user && user.authenticate(params[:session][:password])
+					session[:user_id] = user.id
+					flash[:success] = "You've successfully logged in as #{user.username}"
+					redirect_to root_path
+				else
+					flash.now[:error] = "There was a problem logging in. Please try again."
+					render 'new'
+				end
+		end
+		
+		def destroy
+			session[:user_id] = nil
+			flash[:success] = "You have successfully logged out."
+			redirect_to login_path
+		end
+
 end
