619935455671e80bd84d379d8af2aaa2f655f3de
Brendan McCray <rbmccr@gmail.com>
2019-11-13 12:47:25 -0600

    Instant messaging functionality works

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,18 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      # data sent to coffeescript file
+      ActionCable.server.broadcast "chatroom_channel", item: message_render(message)
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: "message", locals: {message: message})
   end
 
 end
